99330fb3a26bb8a6f8d2148300a95fa129231075
Dmytro Polhul <polhul.d@gmail.com>
2026-01-26 12:52:29 +0200

    fix: alpha release tag

--- vitest.config.js
+++ vitest.config.js
@@ -2,8 +2,20 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    environment: "node",
     globals: true,
     testTimeout: 10000,
     pool: 'threads',
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html", "json-summary"],
+      reportsDirectory: "./coverage",
+      exclude: [
+        "**/node_modules/**",
+        "**/benchmarks/**",
+        "**/js/**",
+        "**/*.d.ts"
+      ]
+    }
   },
 });
